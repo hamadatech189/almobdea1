9f9b0e66d5360a298205d6835d20c7a7ee284399
hamadatech189 <hamadatech189@gmail.com>
2025-07-24 04:39:13 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'almobde3-v1';
+const urlsToCache = [
+  'index.html',
+  'css/style.css',
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
+    })
+  );
+});
